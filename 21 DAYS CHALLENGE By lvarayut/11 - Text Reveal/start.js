7f82e999e049103bb708322fbe11ad2bbc00a081
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-05-27 07:34:37 +0700

    js21 ep12 API
    js21 ep12

--- 21 DAYS CHALLENGE By lvarayut/11 - Text Reveal/start.js
+++ 21 DAYS CHALLENGE By lvarayut/11 - Text Reveal/start.js
@@ -1,4 +1,63 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  /**
+   * Summary Step
+   * ใช้ event scroll 
+   * หาตำแหน่งที่ scroll
+   * ใน event เรียก elem text ที่จะแสดง และ รูปภาพมา
+   * รูปภาพ นำมา คำนวน หา ตำแหน่ง ของ 1/10 
+   * ส่วน text มีการ ทำ style ไว้ ให้ซ่อน (oppacity=0) และ เลื่อนออกไปจาก ตำแหน่งเดิม (transform: translateY(25px)) และ transition: 0.4s all linear 
+   *  เมื่อเวลา add class ใหม่ เข้าไป  แสดงจะได้ดูเหมือนว่าเลื่อน
+   */
+
+  onScroll = () => {
+    const sectionElems = Array.from(document.querySelectorAll('section'));
+    //console.log(sectionElems);
+
+    const scrollY = window.scrollY;
+    console.log(scrollY);
+    
+
+    sectionElems.forEach((sectionElem)=>{
+
+      const imgElem = sectionElem.querySelector('img');
+      const textElem = sectionElem.querySelector('.text');
+      // console.log(imgElems);
+      // console.log(textElems);
+
+      const Postition_scrollYReveal = imgElem.offsetTop + (imgElem.height / 10);
+      if(scrollY >= Postition_scrollYReveal){
+        textElem.classList.add('reveal');
+
+        console.log('offsetHeight = ' + imgElem.offsetHeight);
+        console.log('height = ' + imgElem.height);
+
+      }else{
+        textElem.classList.remove('reveal');
+      }
+
+
+    });
+
+
+
+
+  }
+
+  run = () => {
+    document.addEventListener('scroll', onScroll);
+  }
+
+  run();
+
+
 })();
+
+
+/**
+ * ผู้สอน ใช้ imgElem.offsetHeight  แต่เราใช้ imgElems.height  ไม่ต่างกัน
+ * 
+ * 
+ * 
+ */
---
+++ 21 DAYS CHALLENGE By lvarayut/12 - Air Quality Visualizer/start.js
@@ -0,0 +1,25 @@
+(() => {
+  // เริ่มเขียนโค้ด
+
+  /**
+   * Step
+   * สมัคร API
+   * 
+   * getAirQuality
+   * key fetch ค่าออกมา ใช้ async await
+   * fetch มา ต้องเแปลงเป็น json
+   * ใช้ destructuring ช่วย ดึงค่า ลดการเขียน coding
+   * aqi, tp , hu , ws
+   * 
+   * displayAirQuality
+   * เอา elem แต่ละตัว แสดงผล 
+   * 
+   * setAirQualityColor
+   * css variable
+   * 
+   * Key 9283425b-a890-47f0-bf72-224f16b2749a
+   * AirVisual API
+   * https://api-docs.airvisual.com/?version=latest
+   */
+
+})();
