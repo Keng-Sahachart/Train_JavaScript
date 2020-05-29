8293753d70a5d1fc03061c68e6781b7216494b0e
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-05-29 08:47:34 +0700

    weird part

--- 21 DAYS CHALLENGE By lvarayut/13 - JavaScript Weird Parts-explain ยังไม่ WorkShop/start.js
+++ 21 DAYS CHALLENGE By lvarayut/13 - JavaScript Weird Parts-explain ยังไม่ WorkShop/start.js
@@ -1,7 +1,33 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  // 
+
+  log2 = (text) => {
+    const divDisplayElem = document.querySelector('#Display');
+    divDisplayElem.innerHTML += `${text}</br>`;
+  }
+
+
 
   // 1. NaN
+  /* 
+   * 
+   * 
+   */
+
+  if (NaN === NaN) {
+    log2('NaN === NaN  is true');
+  }else{
+    log2('NaN === NaN  is false');
+  }
+
+  if (Number.isNaN(NaN)) {
+    log2('Number.isNaN(NaN) is true');
+  }else{
+    log2('Number.isNaN(NaN) is false');
+  }
+
+
+
   // 2. Type Coercion
   // 3. Interpreter & Compiler
   // 4. Checking Object Type
